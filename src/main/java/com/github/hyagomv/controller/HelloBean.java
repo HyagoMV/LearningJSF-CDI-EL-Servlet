a756876cc6ea489eb630a07acbae1bd21a388ab1
Hyago M. Vale <hyagomvale@gmail.com>
2021-12-29 09:44:06 -0300

    EL: Lambda 5

--- src/main/java/com/github/hyagomv/controller/HelloBean.java
+++ src/main/java/com/github/hyagomv/controller/HelloBean.java
@@ -2,27 +2,20 @@ package com.github.hyagomv.controller;
 
 import java.io.Serializable;
 
+import javax.el.LambdaExpression;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 @Named("hello")
-@ViewScoped
-public class HelloBean implements Serializable {
-
-	private static final long serialVersionUID = 1L;
+public class HelloBean {
 
 	private boolean show;
 
-	public void foo() {
-		show = !show;
-	}
-
-	public boolean isShow() {
-		return show;
-	}
-
-	public void setShow(boolean show) {
-		this.show = show;
+	public void foo(LambdaExpression lambda, int x) {
+		System.out.println(
+				lambda.invoke(FacesContext.getCurrentInstance().getELContext(), x)
+				);
 	}
 
 }
