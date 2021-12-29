a49c7078234fef75f5806f4f98c4de0b308f701b
Hyago M. Vale <hyagomvale@gmail.com>
2021-12-29 09:55:19 -0300

    JSF: 'actionListener' & 'ActionEvent'

--- src/main/java/com/github/hyagomv/controller/HelloBean.java
+++ src/main/java/com/github/hyagomv/controller/HelloBean.java
@@ -1,21 +1,13 @@
 package com.github.hyagomv.controller;
 
-import java.io.Serializable;
-
-import javax.el.LambdaExpression;
-import javax.faces.context.FacesContext;
-import javax.faces.view.ViewScoped;
+import javax.faces.event.ActionEvent;
 import javax.inject.Named;
 
 @Named("hello")
 public class HelloBean {
 
-	private boolean show;
-
-	public void foo(LambdaExpression lambda, int x) {
-		System.out.println(
-				lambda.invoke(FacesContext.getCurrentInstance().getELContext(), x)
-				);
+	public void foo(ActionEvent event) {
+		System.out.println(event.getSource());
 	}
 
 }
